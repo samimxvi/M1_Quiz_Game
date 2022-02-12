b94f80d6c9434dda2ced9859fad349644d35f13e
DELL-R5 <samimxvi@gmail.com>
2022-02-13 00:30:33 +0530

    multifile

--- 3_Implementation/quiz.c
+++ 3_Implementation/quiz.c
@@ -1,3 +1,6 @@
+// function definitions for quit, know, winner and start
+
+#include "quiz.h"
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
@@ -262,65 +265,5 @@ void start()
             }
             }
         }
-    }
-}
-
-struct choice
-{
-    int ch;
-    int menu;
-};
-
-int main()
-{
-    struct choice c1 = {0, 0};
-
-home:
-    system("cls");
-    printf("\n\n\t\t\t******************@   WELLCOME TO THE QUIZ GAME   @******************\n");
-    printf("\n\t\t\t\t\t\tTOPIC: C PROGRAMMING\n");
-    printf("\n\t\t Press 1 to START the game");
-    printf("\n\n\t\t Press 2 to KNOW how to play ");
-    printf("\n\n\t\t Press 3 to QUIT the game \n");
-    printf("\n\t\t ENTER YOUR OPTION: ");
-    scanf("%d", &c1.ch);
-
-    switch (c1.ch)
-    {
-    case 1:
-        start();
-        printf("\n\n\tEnter 1 to return to the menu: ");
-        scanf("%d", &c1.menu);
-        if (c1.menu == 1)
-        {
-            goto home;
-        }
-        else
-        {
-            printf("\n\n\n\t\tPlease enter valid option!\n\n\n");
-        }
-        break;
-
-    case 2:
-        know();
-        printf("\n\tEnter 1 to return to the menu: ");
-        scanf("%d", &c1.menu);
-        if (c1.menu == 1)
-        {
-            goto home;
-        }
-        else
-        {
-            printf("\n\n\n\t\tPlease enter valid option!\n\n\n");
-        }
-        break;
-
-    case 3:
-        quit();
-        break;
-
-    default:
-        printf("\n\n\n\t\tPlease enter valid option!\n\n\n");
-        break;
     }
 }
---
+++ 3_Implementation/quiz.h
@@ -0,0 +1,11 @@
+#ifndef QUIZ_H_INCLUDED
+#define QUIZ_H_INCLUDED
+
+// Prototypes for the functions
+
+void quit();
+void know();
+void winner(int score, char name[]);
+void start();
+
+#endif
